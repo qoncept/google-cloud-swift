6f37cb82199bf0c2f9652c6893f358ace16a813d
Iceman <okamura@qoncept.co.jp>
2023-09-01 19:47:25 +0900

    remove dead code

--- Sources/GoogleCloud/BigQuery/BigQuery.swift
+++ Sources/GoogleCloud/BigQuery/BigQuery.swift
@@ -155,39 +155,3 @@ public struct BigQuery: Sendable {
         return response
     }
 }
-
-
-@usableFromInline
-@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
-struct AnyAsyncSequence<Element>: Sendable, AsyncSequence {
-    @usableFromInline typealias AsyncIteratorNextCallback = () async throws -> Element?
-
-    @usableFromInline struct AsyncIterator: AsyncIteratorProtocol {
-        @usableFromInline let nextCallback: AsyncIteratorNextCallback
-
-        @inlinable init(nextCallback: @escaping AsyncIteratorNextCallback) {
-            self.nextCallback = nextCallback
-        }
-
-        @inlinable mutating func next() async throws -> Element? {
-            try await self.nextCallback()
-        }
-    }
-
-    @usableFromInline var makeAsyncIteratorCallback: @Sendable () -> AsyncIteratorNextCallback
-
-    @inlinable init<SequenceOfBytes>(
-        _ asyncSequence: SequenceOfBytes
-    ) where SequenceOfBytes: AsyncSequence & Sendable, SequenceOfBytes.Element == Element {
-        self.makeAsyncIteratorCallback = {
-            var iterator = asyncSequence.makeAsyncIterator()
-            return {
-                try await iterator.next()
-            }
-        }
-    }
-
-    @inlinable func makeAsyncIterator() -> AsyncIterator {
-        .init(nextCallback: self.makeAsyncIteratorCallback())
-    }
-}
